4b8c8c342ccd2835d5553ec1ab25a68eabcb3f4e
oharar <bohara@senckenberg.de>
2016-08-04 16:15:51 +0200

    Debugging CreateProjectionGrid() & MakePointsStack()

--- R/MakePointsStack.R
+++ R/MakePointsStack.R
@@ -5,6 +5,7 @@
 #' @param tag Name for tag for the stack (defaults to "points").
 #' @param intercept Boolean, should an intercept be added? Defaults to TRUE.
 #' @param mesh INLA mesh.
+#' @param coordnames Names of coorninates
 #' @param InclCoords Boolean, shoiuld coordinates be included in data (defaults to FALSE).
 #'
 #' @return An INLA stack with points
@@ -13,13 +14,15 @@
 #' @import INLA
 
 MakePointsStack=function(data, presences, tag="points", intercept=TRUE, mesh,
-                         coordnames=c("X","Y"), InclCoords=FALSE) {
+                         coordnames=NULL, InclCoords=FALSE) {
+  if(is.null(coordnames)) coordnames <- colnames(data@coords)
+  if(InclCoords) {
+    data@data[,coordnames] <- data@coords
+  }
   NearestCovs=GetNearestCovariate(points=presences, covs=data)
 
   # Projector matrix from mesh to data.
-  if(InclCoords) {
-    NearestCovs@data[,paste("int",tag,sep=".")] <- 1
-  }
+  NearestCovs@data[,paste("int",tag,sep=".")] <- 1
   projmat <- inla.spde.make.A(mesh, as.matrix(NearestCovs@coords)) # from mesh to point observations
 
   stk.pp <- inla.stack(data=list(y=cbind(rep(1,nrow(NearestCovs)), NA),
