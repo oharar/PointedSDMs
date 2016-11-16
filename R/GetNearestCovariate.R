658110e9801a2dcf724936c73041c1b4849f1ec1
David Lawrence Miller <github@ninepointeightone.net>
2016-11-16 10:30:06 -0500

    don't drop data.frame when selecting only one column

--- R/GetNearestCovariate.R
+++ R/GetNearestCovariate.R
@@ -1,8 +1,9 @@
 #' Function to get covariate values nearest the data/integration point
-# inputs:
+#'
 #' @param points Points for which we need covariates. Either a SpatialPoints* object or a matrix with coordinates in columns.
 #' @param covs Covariates, with coordinates in first and second columns, or a SpatialPointsDataFrame object.
 #' @return A SpatialPointsDataFrame with the original points plus the data from the closest point.
+#'
 #' If there are ties, this will use the first element.
 #'
 #' @export
@@ -18,6 +19,9 @@ GetNearestCovariate <- function(points, covs) {
     dists <- spDistsN1(pts, pt, longlat = TRUE)
     which(dists==min(dists))[1] # need [1] in case min() is not unique. But if it's not, doesn't matter
   }, pts=covs)
-  points.df <- SpatialPointsDataFrame(coords=points@coords, data = covs@data[Nearest,], proj4string = CRS(proj4string(covs)))
-  points.df
+
+  points.df <- SpatialPointsDataFrame(coords=points@coords,
+                                      data = covs@data[Nearest, , drop=FALSE],
+                                      proj4string = CRS(proj4string(covs)))
+  return(points.df)
 }
