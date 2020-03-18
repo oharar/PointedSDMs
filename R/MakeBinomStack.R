4f4f8f0e62d5040a6ca2f90d9c462624bc92a749
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2020-03-18 18:41:26 +0100

    Changed back to original subsetting some places.
    Changed back subsetting all places except where we check class of observs@data[[presname]], as previous changes seemed to cause one error when running the tests. Now I think we will avoid the problems with tibbles, but this also means that the dataframe subsetting is inconsistent throughout the code.

--- R/MakeBinomStack.R
+++ R/MakeBinomStack.R
@@ -36,21 +36,21 @@ MakeBinomStack=function(data, observs, tag="points", intercept=TRUE, mesh, presn
   }
 
   NearestCovs <- GetNearestCovariate(points=observs, covs=data)
-  if(InclCoords) {    data@data[[coordnames]] <- data@coords  }
-  if(intercept) NearestCovs@data[[paste("int",tag,sep=".")]] <- 1 # add intercept
+  if(InclCoords) {    data@data[,coordnames] <- data@coords  }
+  if(intercept) NearestCovs@data[,paste("int",tag,sep=".")] <- 1 # add intercept
   if(!is.null(polynoms)) {
     NearestCovs <- AddDistToRangeToSpatialPoints(data = NearestCovs, polynoms = polynoms, scale=scale)
   }
 
 # If presences are Boolean, reformat
   if(is.logical(observs@data[[presname]])) {
-    observs@data[[presname]] <- as.integer(observs@data[[presname]])
-    observs@data[[trialname]] <- rep(1, nrow(observs@data))
+    observs@data[,presname] <- as.integer(observs@data[,presname])
+    observs@data[,trialname] <- rep(1, nrow(observs@data))
   }
   # Projector matrix from mesh to data.
   projmat <- inla.spde.make.A(mesh, as.matrix(NearestCovs@coords)) # from mesh to point observations
 
-  stk.binom <- inla.stack(data=list(resp=cbind(NA,observs@data[[presname]] ), Ntrials=observs@data[[trialname]]), A=list(1,projmat), tag=tag,
+  stk.binom <- inla.stack(data=list(resp=cbind(NA,observs@data[,presname] ), Ntrials=observs@data[,trialname]), A=list(1,projmat), tag=tag,
                           effects=list(NearestCovs@data, list(i=1:mesh$n)))
 
   stk.binom
